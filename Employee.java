4641c222555dcca7cd47a39d611a1a2d9472ded2
gonzoocampo <64029613+gonzoocampo@users.noreply.github.com>
2020-06-09 18:50:47 -0400

    :)

--- Employee.java
+++ Employee.java
@@ -4,16 +4,39 @@ import java.util.*;
 public class Employee 
 {
     // creating private int donation ID //
-    private int EmployeeID;
+    private static int EmployeeID;
+    private String firstName; 
+    private String lastName; 
+    private String street;
+    private String city; 
+    private String state;
+    private String zipCode; 
+    private String phoneNumber;
+    private String email;
+    private String workStatus;
+    private double wage;    // this is the pay variable 
+    private double hours; 
+    
+    
     // static int set as a different value from other classes 
     private static int nextEmployeeID = 1000;
-    private ArrayList<Employee> allEmployees = new ArrayList<>();
+    
+    public ArrayList<Employee> allEmployees = new ArrayList<>();
     
     public Employee (String firstName, String lastName, String street, 
         String city, String state, String zipCode, String phoneNumber, 
-        String email, String workStatus, double pay) 
+        String email, String workStatus, double wage, double hours) 
     {
-        
-        
+        this.firstName = firstName;
+        this.lastName = lastName; 
+        this.street = street;
+        this.city = city;
+        this.state = state; 
+        this.zipCode = zipCode; 
+        this.phoneNumber = phoneNumber;
+        this.email = email; 
+        this.workStatus = workStatus; 
+        this.wage = wage; 
+        this.hours = hours; 
     }
 }
