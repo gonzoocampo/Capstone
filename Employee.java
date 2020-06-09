866b3200db54e90c2ca9c80c55ad807266305787
gonzoocampo <64029613+gonzoocampo@users.noreply.github.com>
2020-06-09 18:16:16 -0400

    :)

--- Employee.java
+++ Employee.java
@@ -1,6 +1,19 @@
 package CIS484.Capstone;
+import java.util.*;
 
-
-public class Employee {
+public class Employee 
+{
+    // creating private int donation ID
+    private int EmployeeID;
+    // static int set as a different value from other classes 
+    private static int nextEmployeeID = 1000;
+    private ArrayList<Employee> allEmployees = new ArrayList<>();
     
+    public Employee (String firstName, String lastName, String street, 
+        String city, String state, String zipCode, String phoneNumber, 
+        String email, String workStatus, double pay) 
+    {
+        
+        
+    }
 }
